4eea1032c25c439c928e60c268174be029f634f0
ggolden@umich.edu <ggolden@umich.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2006-05-10 16:45:19 +0000

    fixed license header
    git-svn-id: https://source.sakaiproject.org/svn/db/trunk@9228 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- db-impl/ext/src/java/org/sakaiproject/springframework/orm/hibernate/impl/AdditionalHibernateMappingsImpl.java
+++ db-impl/ext/src/java/org/sakaiproject/springframework/orm/hibernate/impl/AdditionalHibernateMappingsImpl.java
@@ -1,24 +1,21 @@
 /**********************************************************************************
- *
- * $Header: /cvs/sakai2/kernel/active-tool-component/src/java/org/sakaiproject/component/kernel/tool/ActiveToolComponent.java,v 1.17 2005/05/26 20:19:27 ggolden.umich.edu Exp $
- *
+ * $URL: https://source.sakaiproject.org/svn/master/trunk/header.java $
+ * $Id: header.java 9220 2006-05-09 23:09:28Z ggolden@umich.edu $
  ***********************************************************************************
  *
- * Copyright (c) 2005 The Regents of the University of Michigan, Trustees of Indiana University,
- *                  Board of Trustees of the Leland Stanford, Jr., University, and The MIT Corporation
- *
- * Licensed under the Educational Community License Version 1.0 (the "License");
- * By obtaining, using and/or copying this Original Work, you agree that you have read,
- * understand, and will comply with the terms and conditions of the Educational Community License.
- * You may obtain a copy of the License at:
- *
- *      http://cvs.sakaiproject.org/licenses/license_1_0.html
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
- * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE
- * AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
- * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
- * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * Copyright (c) 2005, 2006 The Sakai Foundation.
+ * 
+ * Licensed under the Educational Community License, Version 1.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License.
  *
  **********************************************************************************/
 
@@ -26,56 +23,56 @@ package org.sakaiproject.springframework.orm.hibernate.impl;
 
 import java.io.IOException;
 
-import org.hibernate.MappingException;
-import org.hibernate.cfg.Configuration;
-
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.hibernate.MappingException;
+import org.hibernate.cfg.Configuration;
 import org.sakaiproject.springframework.orm.hibernate.AdditionalHibernateMappings;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 
-public class AdditionalHibernateMappingsImpl implements
-    AdditionalHibernateMappings, Comparable
+public class AdditionalHibernateMappingsImpl implements AdditionalHibernateMappings, Comparable
 {
-  protected final transient Log logger = LogFactory.getLog(getClass());
+	protected final transient Log logger = LogFactory.getLog(getClass());
 
-  private Resource[] mappingLocations;
-   private Integer sortOrder = new Integer(Integer.MAX_VALUE);
+	private Resource[] mappingLocations;
 
-  public void setMappingResources(String[] mappingResources)
-  {
-    this.mappingLocations = new Resource[mappingResources.length];
-    for (int i = 0; i < mappingResources.length; i++)
-    {
-      this.mappingLocations[i] = new ClassPathResource(mappingResources[i]
-          .trim());
-    }
-  }
+	private Integer sortOrder = new Integer(Integer.MAX_VALUE);
 
-  public Resource[] getMappingLocations()
-  {
-    return mappingLocations;
-  }
+	public void setMappingResources(String[] mappingResources)
+	{
+		this.mappingLocations = new Resource[mappingResources.length];
+		for (int i = 0; i < mappingResources.length; i++)
+		{
+			this.mappingLocations[i] = new ClassPathResource(mappingResources[i].trim());
+		}
+	}
 
-  public void processConfig(Configuration config) throws IOException,
-      MappingException
-  {
-    for (int i = 0; i < this.mappingLocations.length; i++)
-    {
-      config.addInputStream(this.mappingLocations[i].getInputStream());
-    }
-  }
+	public Resource[] getMappingLocations()
+	{
+		return mappingLocations;
+	}
 
-   public int compareTo(Object o) {
-      return getSortOrder().compareTo(((AdditionalHibernateMappingsImpl)o).getSortOrder());
-   }
+	public void processConfig(Configuration config) throws IOException, MappingException
+	{
+		for (int i = 0; i < this.mappingLocations.length; i++)
+		{
+			config.addInputStream(this.mappingLocations[i].getInputStream());
+		}
+	}
 
-   public Integer getSortOrder() {
-      return sortOrder;
-   }
+	public int compareTo(Object o)
+	{
+		return getSortOrder().compareTo(((AdditionalHibernateMappingsImpl) o).getSortOrder());
+	}
 
-   public void setSortOrder(Integer sortOrder) {
-      this.sortOrder = sortOrder;
-   }
+	public Integer getSortOrder()
+	{
+		return sortOrder;
+	}
+
+	public void setSortOrder(Integer sortOrder)
+	{
+		this.sortOrder = sortOrder;
+	}
 }
